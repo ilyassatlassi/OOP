3be495c18b78e7b593074ad6b86efbee47ca644e
ilyass atlassi <atlassiilyass@gmail.com>
2023-05-17 16:03:00 +0100

    Add decorator

--- person.rb
+++ person.rb
@@ -1,5 +1,7 @@
-# person.rb
-class Person
+require_relative 'nameable'
+
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
@@ -12,6 +14,10 @@ class Person
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
