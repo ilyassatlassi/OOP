30615f14d6e5df3ea4849e80c587e37d3408dd83
ilyass atlassi <atlassiilyass@gmail.com>
2023-05-20 17:39:20 +0100

    Add List of options

--- main.rb
+++ main.rb
@@ -1,4 +1,21 @@
 require_relative 'app'
 
-class main
-    
+def main
+  app = App.new
+
+  loop do
+    puts 'Choose an option:'
+    puts '1. List all books'
+    puts '2. List all people'
+    puts '3. Create a person'
+    puts '4. Create a book'
+    puts '5. Create a rental'
+    puts '6. List all rentals for a given person ID'
+    puts '7. Quit'
+    choose_option(app)
+  end
+end
+
+
+
+main
