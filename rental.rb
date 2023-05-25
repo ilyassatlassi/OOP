cb5fba231c0d21625da209d659d95a3654dedc01
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2023-05-25 14:13:45 +0300

    Add test for classroom

--- rental.rb
+++ rental.rb
@@ -4,7 +4,7 @@ require_relative 'person'
 class Rental
   attr_accessor :date, :person, :book
 
-  def initialize(date, book, person )
+  def initialize(date, book, person)
     @date = date
     @person = person
     @book = book
