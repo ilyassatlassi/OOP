6fdcfbe450cfc73a7c6688cbac78c1a296a93124
ilyass atlassi <atlassiilyass@gmail.com>
2023-05-25 12:56:11 +0100

    Add testing for decorator

--- spec/decorator_spec.rb
+++ spec/decorator_spec.rb
@@ -1,1 +1,20 @@
 
+require_relative 'spec_helper'
+
+describe Decorator do
+  let(:nameable) { double('nameable') }
+  let(:decorator) { Decorator.new(nameable) }
+
+  describe '#initialize' do
+    it 'sets the nameable' do
+      expect(decorator.nameable).to eq(nameable)
+    end
+  end
+
+  describe '#correct_name' do
+    it 'calls the correct_name method on the nameable object' do
+      expect(nameable).to receive(:correct_name)
+      decorator.correct_name
+    end
+  end
+end
